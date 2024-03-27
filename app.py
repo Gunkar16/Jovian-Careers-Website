fa93717ee66f0d11cf9e2d5f431c959aea01a6a7
Gunkar16 <gunkarsinghjohal@gmail.com>
2024-03-27 11:41:01 +0530

    Added API, Added apply Jobs section

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+JOBS = [
+    {
+        'id': 1,
+        'title': "Software Developer",
+        'location': "Noida, India",
+        'salary': "Rs. 20,000,000"
+    },
+    {
+        'id': 2,
+        'title': "Data Scientist",
+        'location': "Delhi, India",
+        'salary': "Rs. 15,000,000"
+    },
+    {
+        'id': 3,
+        'title': "Frontend Engineer",
+        'location': "Chandigarh, India",
+        'salary': "Rs. 10,000,000"
+    },
+    {
+        'id': 4,
+        'title': "Backend Engineer",
+        'location': "Bengaluru, India",
+        'salary': "Rs. 20,000,000"
+    }
+]
 
-app.run(host="0.0.0.0",debug=True)  
-ff
+
+@app.route("/")
+def website():
+    return render_template("index.html",jobs=JOBS)
+@app.route("/api/jobs/")
+def api():
+    return jsonify(JOBS)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
