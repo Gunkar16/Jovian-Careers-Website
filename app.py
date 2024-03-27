4288d6cde6458f25f4f78ada1678b1fa1ef0899c
Gunkar16 <gunkarsinghjohal@gmail.com>
2024-03-27 11:44:09 +0530

    Removed spaces

--- app.py
+++ app.py
@@ -33,9 +33,11 @@ JOBS = [
 @app.route("/")
 def website():
     return render_template("index.html",jobs=JOBS)
+
 @app.route("/api/jobs/")
 def api():
     return jsonify(JOBS)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
